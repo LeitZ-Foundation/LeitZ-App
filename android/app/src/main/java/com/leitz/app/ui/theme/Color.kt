58d5f7ec65829a871354bd3ec20820a20d5cbf94
Davi <languith54@gmail.com>
2026-08-21 12:14:28 -0300

    Add Color.kt with theme color definitions

---
+++ android/app/src/main/java/com/leitz/app/ui/theme/Color.kt
@@ -0,0 +1,13 @@
+package com.leitz.app.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val LeitXPrimary = Color(0xFF00C853)
+val LeitXSecondary = Color(0xFF00BFA5)
+val LeitXDark = Color(0xFF121212)
+val LeitXSurface = Color(0xFF1E1E1E)
+val LeitXTextPrimary = Color(0xFFFFFFFF)
+val LeitXTextSecondary = Color(0xFFB0BEC5)
+val LeitXAccent = Color(0xFFFF6D00)
+val LeitXError = Color(0xFFCF6679)
+val LeitXBackground = Color(0xFF0A0A0A)
